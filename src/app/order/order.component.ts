a9d82488d7938537f5dbaf769978ed2e6a2b0a87
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2020-08-05 00:27:22 -0300

    Validadores Personalizados

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  AbstractControl,
+} from "@angular/forms";
 import { RadioOptions } from "app/shared/radio/radio-options.model";
 import { OrderService } from "./order.service";
 import { CartItem } from "app/restaurant-detail/shopping-cart/cart-item.model";
@@ -47,37 +52,81 @@ export class OrderComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.orderForm = this.formBuilder.group({
-      /** Dentro deste objeto, vou ter propriedades que representam
-       * os imputs do formulário, e testaremos uma válidação personalizada
-       * Temos duas formas para fazer a primeira é:
-       * 1 - Podemos colocar o valor da propriedade dentro do objeto name: ''  ou
-       * 2 - Utilizar um método do formBuilder que cria um componente pra gente
-       * que é um método chamado control -> email: this.formBuilder.control('')
-       */
-      name: this.formBuilder.control("", [
-        Validators.required,
-        Validators.minLength(5),
-      ]),
-      email: this.formBuilder.control("", [
-        Validators.required,
-        Validators.pattern(this.emailPattern),
-      ]),
-      emailConfirmation: this.formBuilder.control("", [
-        Validators.required,
-        Validators.pattern(this.emailPattern),
-      ]),
-      address: this.formBuilder.control("", [
-        Validators.required,
-        Validators.minLength(5),
-      ]),
-      number: this.formBuilder.control("", [
-        Validators.required,
-        Validators.pattern(this.numberPattern),
-      ]),
-      optionalAddress: this.formBuilder.control(""),
-      paymentOption: this.formBuilder.control("", [Validators.required]),
-    });
+    this.orderForm = this.formBuilder.group(
+      {
+        /** Dentro deste objeto, vou ter propriedades que representam
+         * os imputs do formulário, e testaremos uma válidação personalizada
+         * Temos duas formas para fazer a primeira é:
+         * 1 - Podemos colocar o valor da propriedade dentro do objeto name: ''  ou
+         * 2 - Utilizar um método do formBuilder que cria um componente pra gente
+         * que é um método chamado control -> email: this.formBuilder.control('')
+         */
+        name: this.formBuilder.control("", [
+          Validators.required,
+          Validators.minLength(5),
+        ]),
+        email: this.formBuilder.control("", [
+          Validators.required,
+          Validators.pattern(this.emailPattern),
+        ]),
+        emailConfirmation: this.formBuilder.control("", [
+          Validators.required,
+          Validators.pattern(this.emailPattern),
+        ]),
+        address: this.formBuilder.control("", [
+          Validators.required,
+          Validators.minLength(5),
+        ]),
+        number: this.formBuilder.control("", [
+          Validators.required,
+          Validators.pattern(this.numberPattern),
+        ]),
+        optionalAddress: this.formBuilder.control(""),
+        paymentOption: this.formBuilder.control("", [Validators.required]),
+      },
+      { validator: OrderComponent.equalsTo }
+    );
+
+    /** Em Reactive Forms podemor criar validadores e associar aos grupos que estamos formando
+     *  Então a gente tem um grupo que criamos que representa o form inteiro, criado através da função
+     *  group do formBuilder e ela aceita alguns parametros, um desses parametros é o validador
+     *  capaz de ter uma referência de todos os campos do grupo e ai fazer uma validação
+     *  checando valores de multiplos campos
+     */
+  }
+
+  /**
+   * @param group
+   * Tipo AbstracControl que retorna um objeto cujo a chave é do tipo string
+   * e o retorno vai ser do tipo boolean.
+   * Quando a gente fizer uma validação a gente retorna uma chave e o valor da chave
+   * a gente pode escolher como ex: emailsNotMatch e a gente passa um valor booleano
+   * e podemos usar esse valor dentro do template para dar um feedback visual.
+   *
+   * O que esta função faz?
+   * Ela vai checkar o valor de dois campos email e emailConfirmation, então
+   * a gente recebe o group para gente poder checar e através do metodo get() podemos
+   * obter uma referência do input que quisermos através do nome da propriedade do objeto.   *
+   */
+  static equalsTo(group: AbstractControl): { [key: string]: boolean } {
+    const email = group.get("email");
+    const emailConfirmation = group.get("emailConfirmation");
+
+    /** Checamos se existe dentro do grupo */
+    if (!email || !emailConfirmation) {
+      return undefined;
+    }
+
+    /** Caso eles existam a gente checa os valore, se os valores forem diferente retornamos uma chave
+     * essa chave é subjetiva, vc pode criar a chave que quiser contanto que ela te auxilie
+     * a dar um feedback visual
+     */
+    if (email.value !== emailConfirmation.value) {
+      return { emailsNotMatch: true };
+    }
+
+    /** Caso os valores batam eu retorno undefined e não associo essa chave a grupo nenhum  */
+    return undefined;
   }
 
   itemsValue(): number {
