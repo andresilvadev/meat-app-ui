173c4696c4f3c4104b1c388e933a0f69c4c691d8
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2020-08-16 14:58:18 -0300

    CoreModule - Criando core module com nossos servicos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,21 +11,17 @@ import { HomeComponent } from "./home/home.component";
 
 import { RestaurantsComponent } from "./restaurants/restaurants.component";
 import { RestaurantComponent } from "./restaurants/restaurant/restaurant.component";
-import { RestaurantsService } from "./restaurants/restaurants.service";
+
 import { RestaurantDetailComponent } from "./restaurant-detail/restaurant-detail.component";
 import { MenuComponent } from "./restaurant-detail/menu/menu.component";
 import { ShoppingCartComponent } from "./restaurant-detail/shopping-cart/shopping-cart.component";
 import { MenuItemComponent } from "./restaurant-detail/menu-item/menu-item.component";
 import { ReviewsComponent } from "./restaurant-detail/reviews/reviews.component";
-import { ShoppingCartService } from "./restaurant-detail/shopping-cart/shopping-cart.service";
-import { OrderComponent } from "./order/order.component";
 
-import { OrderItemsComponent } from "./order/order-items/order-items.component";
-import { OrderService } from "./order/order.service";
-import { DeliveryCostsComponent } from "./order/delivery-costs/delivery-costs.component";
 import { OrderSummaryComponent } from "./order-summary/order-summary.component";
 
 import { SharedModule } from "./shared/shared.module";
+import { CoreModule } from "./core/core.module";
 
 @NgModule({
   declarations: [
@@ -47,12 +43,10 @@ import { SharedModule } from "./shared/shared.module";
     BrowserModule,
     HttpModule,
     SharedModule,
+    CoreModule,
     RouterModule.forRoot(ROUTES),
   ],
   providers: [
-    RestaurantsService,
-    ShoppingCartService,
-    OrderService,
     {
       provide: LOCALE_ID,
       useValue: "pt-BR",
