7fa2bb6a565d9331512f6a623f1d3808463e762d
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2020-11-04 11:27:51 -0300

    Usando Animação com Keyframes nos Itens do Carrinho

--- src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -1,12 +1,38 @@
 import { Component, OnInit } from "@angular/core";
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
+
 import { ShoppingCartService } from "./shopping-cart.service";
 
 @Component({
   selector: "mt-shopping-cart",
   templateUrl: "./shopping-cart.component.html",
   styleUrls: ["./shopping-cart.component.css"],
+  animations: [
+    trigger('row', [
+      state('ready', style({ opacity: 1 })),
+      transition('void => ready', animate('300ms 0s ease-in', keyframes([
+        /**
+         * opacity: 0 = Começa invisivel
+         * transform: translateX(-30) = por que vamos mexer somente nas laterais
+         * offset: 0 = Onde dentro da animação o keyframe vai estar, offset marca o keyframe
+         * offset 0 é o primeiro ponto da animação
+         */
+        style({opacity:0, transform: 'translateX(-30px)', offset: 0}),
+        style({opacity:0.8, transform: 'translateX(10px)', offset: 0.8}),
+        style({opacity:1, transform: 'translateX(0px)', offset: 1})
+      ]))),
+      transition('ready => void', animate('300ms 0s ease-out', keyframes([       
+        style({opacity:1, transform: 'translateX(0px)', offset: 0}),
+        style({opacity:0.8, transform: 'translateX(-10px)', offset: 0.2}),
+        style({opacity:0, transform: 'translateX(30px)', offset: 1})
+      ])))
+    ])
+  ]
 })
 export class ShoppingCartComponent implements OnInit {
+
+  rowState = 'ready'
+
   constructor(public shoppingCartService: ShoppingCartService) {}
 
   ngOnInit() {}
