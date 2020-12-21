b1b40fdd9f3620b38c5081f64e103cad1cd370fb
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2020-12-21 17:05:31 -0300

    114. Adicionando Token JWT a Rota de Login

--- backend/dist/auth.js
+++ backend/dist/auth.js
@@ -2,13 +2,19 @@
 exports.__esModule = true;
 exports.handleAuthentication = void 0;
 var users_1 = require("./users");
+var jwt = require("jsonwebtoken");
 var handleAuthentication = function (req, resp) {
     var user = req.body;
     if (isValid(user)) {
         var dbUser = users_1.users[user.email];
+        var token = jwt.sign({
+            sub: dbUser.email,
+            iss: 'meat-api'
+        }, 'meat-api-password');
         resp.json({
             name: dbUser.name,
-            email: dbUser.email
+            email: dbUser.email,
+            accessToken: token
         });
     }
     else {
