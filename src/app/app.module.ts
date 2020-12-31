dec9bf442872905d90eb662f84eb848c767d2cef
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2020-12-30 22:07:41 -0300

    126. Criando um Error Handler Global

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from "@angular/platform-browser";
-import { NgModule, LOCALE_ID } from "@angular/core";
+import { NgModule, LOCALE_ID, ErrorHandler } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { RouterModule, PreloadAllModules } from "@angular/router";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
@@ -26,6 +26,7 @@ import { SharedModule } from "./shared/shared.module";
 import { NotFoundComponent } from './not-found/not-found.component';
 import { LoginComponent } from './security/login/login.component';
 import { UserDetailComponent } from './header/user-detail/user-detail.component';
+import { ApplicationErrorHandler } from "./app.error-handler";
 
 @NgModule({
   declarations: [
@@ -72,6 +73,10 @@ import { UserDetailComponent } from './header/user-detail/user-detail.component'
       provide: LOCALE_ID,
       useValue: "pt-BR",
     },
+    {
+      provide: ErrorHandler, 
+      useClass: ApplicationErrorHandler
+    }
   ],
   bootstrap: [AppComponent],
 })
